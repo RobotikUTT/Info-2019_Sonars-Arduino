4a8b235655506270b11a5d2fa06addabe698ce74
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2019-05-29 18:41:43 +0200

    cleaned libs and managed lcd screen

--- src/main.cpp
+++ src/main.cpp
@@ -10,39 +10,85 @@
 #include <SonarArray.hpp>
 
 //oled i2c screen :
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <LiquidCrystal_I2C.h>
 
 //CAN :
 #include <arduino/serial.hpp>
 
+// Sonars functions and objects
 void sendSonarsValueToCan();
-
-SonarArray sonarArray(NB_SONARS, SONAR_ECHO_FRONT_L, SONAR_TRIG_FRONT_L,
-                                 SONAR_ECHO_FRONT_R, SONAR_TRIG_FRONT_R,
-                                 SONAR_ECHO_L, SONAR_TRIG_L,
-                                 SONAR_ECHO_R, SONAR_TRIG_R,
-                                 SONAR_ECHO_BACK, SONAR_TRIG_BACK);
-
-SerialHandler serial;
+SonarArray sonarArray(NB_SONARS,
+  SONAR_ECHO_FRONT_L,SONAR_TRIG_FRONT_L,
+  SONAR_ECHO_FRONT_R, SONAR_TRIG_FRONT_R,
+  SONAR_ECHO_L, SONAR_TRIG_L,
+  SONAR_ECHO_R, SONAR_TRIG_R,
+  SONAR_ECHO_BACK, SONAR_TRIG_BACK
+);
 unsigned long lastMillis = 0;
 
-void setup() {
-  Serial.begin(57600);
-  SPI.begin();
+// Serial handler for frames coming from serial
+SerialHandler serialHandler;
 
-  serial.setup();
+// OLED display (width, height, I2C, reset pin = -1 for sharing arduino reset pin)
+LiquidCrystal_I2C display(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup() {
+  // Serial frames
+  serialHandler.begin(57600);
+
+  // LCD screen
+  display.init();
+
+  // Print a message to the LCD.
+  display.backlight();
+  display.setCursor(0, 0);
+  display.print("waiting for master :)");
 }
 
 void loop() {
   sonarArray.update();
 
-  auto frame = serial.read();
-	if (serial.is(frame, UPDATE_SCREEN)) {
+  std::vector<int> frame = serialHandler.read();
+
+	if (serialHandler.is(frame, UPDATE_SCREEN)) {
     // First argument
     int points = frame[1];
 
-		// TODO : Set screen points here
+    display.setCursor(0, 0);
+
+    display.print("*");
+
+    if (points < 100) {
+      display.print("0");
+      if (points < 10) {
+        display.print("0");
+      }
+    }
+
+    display.print(points);
+    display.print("*        ");
+
+    // Beautitul smileys
+    switch (frame[2]) {
+    case 0: // booting
+      display.print("@_-");
+      break;
+    case 1: // ready
+      display.print(" o/");
+      break;
+    case 2: // running
+      display.print("/o/");
+      break;
+    case 3: // error
+      display.print("x_x");
+      break;
+    case 4: // done
+      display.print(">;)");
+      break;
+    default:
+      display.print("wtf");
+      break;
+    }
 	}
 
   if (lastMillis + 200 < millis()) {
@@ -54,5 +100,5 @@ void loop() {
 void sendSonarsValueToCan()
 {
   std::vector<uint8_t> measures = sonarArray.getDistancesCM();
-  serial.send(SONAR_DISTANCE, measures[0], measures[1], measures[2], measures[3], measures[4]);
+  serialHandler.send(SONAR_DISTANCE, measures[0], measures[1], measures[2], measures[3], measures[4]);
 }
